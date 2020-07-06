4c099dd474d9a9c4d54fcda7ec30546ba7905eb5
Hanif Bin Ariffin <hanif.ariffin.4326@gmail.com>
2020-07-05 22:44:36 -0400

    Some more changes...
    1. Renamed DEFINE_* => DECLARE_* (In the future, the definition might be provided as a separate C file)
    2. Ability to initialize Vec with iota.

--- include/aoihana.h
+++ include/aoihana.h
@@ -5,11 +5,11 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-#define DEFINE_FOLD_FUNCTION_SIGNATURE(type) \
+#define DECLARE_FOLD_FUNCTION_SIGNATURE(type) \
   typedef type (*type##_fold_function_sig)(const type, const type);
 
-#define DEFINE_FOLD(type)                                          \
-  DEFINE_FOLD_FUNCTION_SIGNATURE(type)                             \
+#define DECLARE_FOLD(type)                                         \
+  DECLARE_FOLD_FUNCTION_SIGNATURE(type)                            \
   int fold_##type(const type *arr, const int size, const int init, \
                   const type##_fold_function_sig f)                \
   {                                                                \
@@ -21,11 +21,11 @@
     return acc;                                                    \
   }
 
-#define DEFINE_FOREACH_FUNCTION_SIGNATURE(type) \
+#define DECLARE_FOREACH_FUNCTION_SIGNATURE(type) \
   typedef void (*type##_foreach_function_sig)(const type);
 
-#define DEFINE_FOREACH(type)                           \
-  DEFINE_FOREACH_FUNCTION_SIGNATURE(type)              \
+#define DECLARE_FOREACH(type)                          \
+  DECLARE_FOREACH_FUNCTION_SIGNATURE(type)             \
   int fold_##type(const type *arr, const int size,     \
                   const type##_foreach_function_sig f) \
   {                                                    \
@@ -36,10 +36,13 @@
     return acc;                                        \
   }
 
-#define DEFINE_VEC_FOLD_FUNCTION_SIGNATURE(type) \
+#define DECLARE_VEC_FOLD_FUNCTION_SIGNATURE(type) \
   typedef type (*vec_##type##_fold_function_sig)(type, type);
 
-#define DEFINE_VEC_RESULT_TYPE(type)                                \
+#define DECLARE_VEC_IOTA_FUNCTION_SIGNATURE(type) \
+  typedef type (*vec_##type##_iota_function_sig)(type);
+
+#define DECLARE_VEC_RESULT_TYPE(type)                               \
   typedef struct Vec_Result_##type##_                               \
   {                                                                 \
     bool success;                                                   \
@@ -60,67 +63,85 @@
     return result;                                                  \
   }
 
-#define DEFINE_VEC(type)                                                   \
-  DEFINE_VEC_RESULT_TYPE(type)                                             \
-  typedef struct Vec_##type##_                                             \
-  {                                                                        \
-    type *ptr;                                                             \
-    int len;                                                               \
-    int capacity;                                                          \
-  } Vec_##type;                                                            \
-  Vec_##type vec_##type##_new()                                            \
-  {                                                                        \
-    Vec_##type vec;                                                        \
-    vec.ptr = (type *)malloc(1 * sizeof(type));                            \
-    vec.len = 0;                                                           \
-    vec.capacity = 1;                                                      \
-    return vec;                                                            \
-  }                                                                        \
-  Vec_##type vec_##type##_with_capacity(const int capacity)                \
-  {                                                                        \
-    Vec_##type vec;                                                        \
-    vec.ptr = (type *)malloc(capacity * sizeof(type));                     \
-    vec.len = 0;                                                           \
-    vec.capacity = capacity;                                               \
-    return vec;                                                            \
-  }                                                                        \
-  void vec_##type##_push_back(Vec_##type *vec, const type x)               \
-  {                                                                        \
-    if (vec->len == vec->capacity)                                         \
-    {                                                                      \
-      int new_cap = vec->capacity * 2 + 1;                                 \
-      type *new_array = (type *)realloc(vec->ptr, new_cap * sizeof(type)); \
-      vec->ptr = new_array;                                                \
-      vec->capacity = new_cap;                                             \
-    }                                                                      \
-    vec->ptr[vec->len] = x;                                                \
-    vec->len = vec->len + 1;                                               \
-  }                                                                        \
-  void vec_##type##_destroy(Vec_##type *vec)                               \
-  {                                                                        \
-    free(vec->ptr);                                                        \
-    vec->ptr = NULL;                                                       \
-    vec->len = 0;                                                          \
-  }                                                                        \
-  DEFINE_VEC_FOLD_FUNCTION_SIGNATURE(type);                                \
-  type vec_##type##_fold(Vec_##type *vec, const type init,                 \
-                         vec_##type##_fold_function_sig f)                 \
-  {                                                                        \
-    return fold_##type(vec->ptr, vec->len, init, f);                       \
-  }                                                                        \
-  Vec_Result_##type vec_##type##_at(Vec_##type *vec, const int index)      \
-  {                                                                        \
-    if (index < vec->len)                                                  \
-    {                                                                      \
-      return vec_##type##_result_create_ok(*(vec->ptr + index));           \
-    }                                                                      \
-    else                                                                   \
-    {                                                                      \
-      return vec_##type##_result_create_error();                           \
-    }                                                                      \
+#define DECLARE_VEC(type)                                                   \
+  DECLARE_VEC_RESULT_TYPE(type)                                             \
+  typedef struct Vec_##type##_                                              \
+  {                                                                         \
+    type *ptr;                                                              \
+    int len;                                                                \
+    int capacity;                                                           \
+  } Vec_##type;                                                             \
+  void vec_##type##_push_back(Vec_##type *vec, const type x);               \
+  /** Everything below this is actually the definition.*/                   \
+  Vec_##type vec_##type##_new()                                             \
+  {                                                                         \
+    Vec_##type vec;                                                         \
+    vec.ptr = (type *)malloc(1 * sizeof(type));                             \
+    vec.len = 0;                                                            \
+    vec.capacity = 1;                                                       \
+    return vec;                                                             \
+  }                                                                         \
+  Vec_##type vec_##type##_with_capacity(const int capacity)                 \
+  {                                                                         \
+    Vec_##type vec;                                                         \
+    vec.ptr = (type *)malloc(capacity * sizeof(type));                      \
+    vec.len = 0;                                                            \
+    vec.capacity = capacity;                                                \
+    return vec;                                                             \
+  }                                                                         \
+  DECLARE_VEC_IOTA_FUNCTION_SIGNATURE(type);                                \
+  Vec_##type vec_##type##_with_iota(const int capacity, const type init,    \
+                                    const vec_##type##_iota_function_sig f) \
+  {                                                                         \
+    Vec_##type vec;                                                         \
+    vec.ptr = (type *)malloc(capacity * sizeof(type));                      \
+    vec.len = 0;                                                            \
+    vec.capacity = capacity;                                                \
+    type iota = init;                                                       \
+    for (int index = 0; index != capacity; index++)                         \
+    {                                                                       \
+      vec_##type##_push_back(&vec, iota);                                   \
+      iota = f(iota);                                                       \
+    }                                                                       \
+    return vec;                                                             \
+  }                                                                         \
+  void vec_##type##_push_back(Vec_##type *vec, const type x)                \
+  {                                                                         \
+    if (vec->len == vec->capacity)                                          \
+    {                                                                       \
+      int new_cap = vec->capacity * 2 + 1;                                  \
+      type *new_array = (type *)realloc(vec->ptr, new_cap * sizeof(type));  \
+      vec->ptr = new_array;                                                 \
+      vec->capacity = new_cap;                                              \
+    }                                                                       \
+    vec->ptr[vec->len] = x;                                                 \
+    vec->len = vec->len + 1;                                                \
+  }                                                                         \
+  void vec_##type##_destroy(Vec_##type *vec)                                \
+  {                                                                         \
+    free(vec->ptr);                                                         \
+    vec->ptr = NULL;                                                        \
+    vec->len = 0;                                                           \
+  }                                                                         \
+  DECLARE_VEC_FOLD_FUNCTION_SIGNATURE(type);                                \
+  type vec_##type##_fold(Vec_##type *vec, const type init,                  \
+                         vec_##type##_fold_function_sig f)                  \
+  {                                                                         \
+    return fold_##type(vec->ptr, vec->len, init, f);                        \
+  }                                                                         \
+  const Vec_Result_##type vec_##type##_at(Vec_##type *vec, const int index) \
+  {                                                                         \
+    if (index < vec->len)                                                   \
+    {                                                                       \
+      return vec_##type##_result_create_ok(*(vec->ptr + index));            \
+    }                                                                       \
+    else                                                                    \
+    {                                                                       \
+      return vec_##type##_result_create_error();                            \
+    }                                                                       \
   }
 
-#define DEFINE_ARITHMETIC_OPERATIONS(type)             \
+#define DECLARE_ARITHMETIC_OPERATIONS(type)            \
   type plus_##type(const type lhs, const type rhs)     \
   {                                                    \
     return lhs + rhs;                                  \
@@ -138,10 +159,10 @@
     return lhs / rhs;                                  \
   }
 
-#define IMPLEMENT_FOR_TYPE(type)      \
-  DEFINE_ARITHMETIC_OPERATIONS(type); \
-  DEFINE_FOLD(type);                  \
-  DEFINE_VEC(type);
+#define IMPLEMENT_FOR_TYPE(type)       \
+  DECLARE_ARITHMETIC_OPERATIONS(type); \
+  DECLARE_FOLD(type);                  \
+  DECLARE_VEC(type);
 
 IMPLEMENT_FOR_TYPE(char);
 IMPLEMENT_FOR_TYPE(int);
