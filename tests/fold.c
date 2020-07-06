64452e3b8768ead139148308bbd16bbcc1feb083
Hanif Bin Ariffin <hanif.ariffin.4326@gmail.com>
2020-07-05 21:55:52 -0400

    Updated test.

--- tests/fold.c
+++ tests/fold.c
@@ -2,14 +2,16 @@
 
 #include "../include/aoihana.h"
 
+const int TEST_VEC_SIZE = 30;
+
 int main(int argc, char **argv)
 {
-    Vec_int vec = vec_int_with_capacity(30);
-    for (int c = 0; c != 30; c++)
+    Vec_int vec = vec_int_with_capacity(TEST_VEC_SIZE);
+    for (int c = 0; c != TEST_VEC_SIZE; c++)
     {
         vec_int_push_back(&vec, 1);
     }
     const int result = fold_int(vec.ptr, vec.len, 0, plus_int);
-    assert(result == 30);
+    assert(result == TEST_VEC_SIZE);
     vec_int_destroy(&vec);
 }
