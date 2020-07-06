4dee92515ad14ff2498838e6153421169e8134cd
Hanif Bin Ariffin <hanif.ariffin.4326@gmail.com>
2020-07-05 22:48:56 -0400

    Updated test

--- tests/vec.c
+++ tests/vec.c
@@ -4,6 +4,55 @@
 
 const int TEST_VEC_SIZE = 30;
 
+#define TEST_ACCESS_AND_VALUE(vec, index, value) assert(vec_int_at(&vec, index).success == true && *vec_int_at(&vec, index).ptr == value);
+
+void test_removing_element_based_on_index()
+{
+    Vec_int vec = vec_int_with_iota(5, 0, successor_int);
+
+    for (int index = 0; index != 5; index++)
+    {
+        TEST_ACCESS_AND_VALUE(vec, 0, 0);
+        TEST_ACCESS_AND_VALUE(vec, 1, 1);
+        TEST_ACCESS_AND_VALUE(vec, 2, 2);
+        TEST_ACCESS_AND_VALUE(vec, 3, 3);
+        TEST_ACCESS_AND_VALUE(vec, 4, 4);
+    }
+
+    const int initial_capacity = vec.capacity;
+
+    assert(vec_int_remove(&vec, 3) == true);
+
+    {
+        TEST_ACCESS_AND_VALUE(vec, 0, 0);
+        TEST_ACCESS_AND_VALUE(vec, 1, 1);
+        TEST_ACCESS_AND_VALUE(vec, 2, 2);
+        TEST_ACCESS_AND_VALUE(vec, 3, 4);
+    }
+
+    assert(vec_int_remove(&vec, 0) == true);
+
+    {
+        TEST_ACCESS_AND_VALUE(vec, 0, 1);
+        TEST_ACCESS_AND_VALUE(vec, 1, 2);
+        TEST_ACCESS_AND_VALUE(vec, 2, 4);
+    }
+
+    assert(vec_int_remove(&vec, 1) == true);
+
+    {
+        TEST_ACCESS_AND_VALUE(vec, 0, 1);
+        TEST_ACCESS_AND_VALUE(vec, 1, 4);
+    }
+
+    assert(vec_int_remove(&vec, 0) == true);
+    assert(vec_int_remove(&vec, 0) == true);
+
+    assert(vec.len == 0);
+    assert(vec.capacity == initial_capacity);
+}
+
+// TODO: Refactor these tests out into their own functions...
 int main(int argc, char **argv)
 {
     /// Create with capacity.
@@ -19,27 +68,13 @@ int main(int argc, char **argv)
     for (int c = 0; c != TEST_VEC_SIZE; c++)
     {
         const Vec_Result_int result = vec_int_at(&vec, c);
-        if (result.success)
-        {
-            assert(*result.ptr == c);
-        }
-        else
-        {
-            assert(false);
-        }
+        assert(result.success && *result.ptr == c);
     }
 
     /// Check that access out of index will yield a negative result.
     {
         Vec_Result_int result = vec_int_at(&vec, TEST_VEC_SIZE);
-        if (result.success)
-        {
-            assert(false);
-        }
-        else
-        {
-            assert(true);
-        }
+        assert(result.success == false);
     }
 
     /// Create with iota.
@@ -50,14 +85,7 @@ int main(int argc, char **argv)
     {
         Vec_Result_int result_iota = vec_int_at(&vec_iota, c);
         Vec_Result_int result_vec = vec_int_at(&vec, c);
-        if (result_iota.success && result_vec.success)
-        {
-            assert(*result_iota.ptr == *result_vec.ptr);
-        }
-        else
-        {
-            assert(false);
-        }
+        assert(result_iota.success && result_vec.success && *result_iota.ptr == *result_vec.ptr);
     }
 
     /// Test folding over Vec calculating the sum.
@@ -67,6 +95,7 @@ int main(int argc, char **argv)
         assert(free_fold_result == vec_fold_result);
     }
 
+    test_removing_element_based_on_index();
     /// Cleanup!
     vec_int_destroy(&vec);
     vec_int_destroy(&vec_iota);
